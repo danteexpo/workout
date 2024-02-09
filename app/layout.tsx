2d5d8bffb5c16dbe2ee2effd3a696801dc06d5ff
Dante <dante0im@gmail.com>
2024-02-09 16:27:19 -0300

    Fixed fonts and passed to Supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata, Viewport } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 
-const fontSans = FontSans({
+export const inter = Inter({
 	subsets: ["latin"],
-	variable: "--font-sans",
+	variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -30,7 +30,7 @@ export default function RootLayout({
 			<body
 				className={cn(
 					"min-h-screen bg-background font-sans antialiased p-6 md:p-8",
-					fontSans.variable
+					inter.variable
 				)}
 			>
 				<ThemeProvider
